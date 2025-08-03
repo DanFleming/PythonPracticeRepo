2f1ea3d14bb4e66877d57e4a6724061c6641db41
DanFleming <flemingdaniel@rocketmail.com>
2025-08-03 13:43:55 +0200

    resolving case issue

--- quiz game.py
+++ quiz game.py
@@ -2,36 +2,36 @@ print("Welcome to the quiz!")
 
 playing = input("Do you want to play? (y or n) ")
 
-if playing.lower != "y":
+if playing != "y":
     quit()
 
 print("Okay! Let's play!")
 
 score = 0
 
-answer = input("What does CPU stand for? ")
-if answer.lower == "central processing unit":
+answer = input("What does CPU stand for? ").lower()
+if answer == "central processing unit":
     score += 1
     print("correct!")
 else:
     print("incorrect!")
 
-answer = input("What does GPU stand for? ")
-if answer.lower == "graphics processing unit":
+answer = input("What does GPU stand for? ").lower()
+if answer == "graphics processing unit":
     score += 1
     print("correct!")
 else:
     print("incorrect!")
 
-answer = input("What does RAM stand for? ")
-if answer.lower == "random access memory":
+answer = input("What does RAM stand for? ").lower()
+if answer == "random access memory":
     score += 1
     print("correct!")
 else:
     print("incorrect!")
 
-answer = input("What does PSU stand for? ")
-if answer.lower == "power supply":
+answer = input("What does PSU stand for? ").lower()
+if answer == "power supply":
     score += 1
     print("correct!")
 else:
