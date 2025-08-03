c3b5af22bc0173526162f69891e8eec58b994668
DanFleming <flemingdaniel@rocketmail.com>
2025-08-03 13:27:53 +0200

    Quiz game start

---
+++ quiz game.py
@@ -0,0 +1,41 @@
+print("Welcome to the quiz!")
+
+playing = input("Do you want to play? (y or n) ")
+
+if playing.lower != "y":
+    quit()
+
+print("Okay! Let's play!")
+
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.lower == "central processing unit":
+    score += 1
+    print("correct!")
+else:
+    print("incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower == "graphics processing unit":
+    score += 1
+    print("correct!")
+else:
+    print("incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer.lower == "random access memory":
+    score += 1
+    print("correct!")
+else:
+    print("incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer.lower == "power supply":
+    score += 1
+    print("correct!")
+else:
+    print("incorrect!")
+
+print("You got " + str(score) + " questions right!")
+print("That's " + ((score / 4) * 100) + "%")
